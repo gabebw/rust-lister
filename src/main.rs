5ddc23bf329a0255890efdc39505eaee51289b12
Gabe Berke-Williams <gabebw@gabebw.com>
2019-08-23 08:14:48 -0700

    Factor out a method

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::cmp::{self, Reverse};
 use std::env;
 use std::io::{self, ErrorKind};
+use std::path::{PathBuf};
 use std::time::{SystemTime};
 use walkdir::{DirEntry, WalkDir};
 
@@ -31,6 +32,21 @@ fn is_file(entry: &DirEntry) -> bool {
     entry.file_type().is_file()
 }
 
+fn build_entries(current_dir: &PathBuf) -> io::Result<Vec<Entry>> {
+    let mut entries: Vec<Entry> = vec!();
+
+    let walker = WalkDir::new(&current_dir).follow_links(true);
+    // Don't filter `is_file` in `filter_entry` because then it doesn't descend into directories
+    for direntry in walker.into_iter().filter_entry(|e| !is_hidden(e)) {
+        let direntry = direntry?;
+        if is_file(&direntry) {
+            let entry = build_entry(&direntry)?;
+            entries.push(entry);
+        }
+    }
+    Ok(entries)
+}
+
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
     let maximum_number_of_entries_to_print: usize = if args.len() < 2 {
@@ -40,23 +56,7 @@ fn main() -> io::Result<()> {
     };
 
     let current_dir = env::current_dir()?;
-    let mut entries: Vec<Entry> = vec!();
-
-    let walker = WalkDir::new(&current_dir).follow_links(true);
-    // Don't filter `is_file` in `filter_entry` because then it doesn't descend into directories
-    for direntry in walker.into_iter().filter_entry(|e| !is_hidden(e)) {
-        if let Ok(direntry) = direntry {
-            if is_file(&direntry) {
-                if let Ok(entry) = build_entry(&direntry) {
-                    entries.push(entry);
-                } else {
-                    eprintln!("Failed building entry");
-                }
-            }
-        } else {
-            eprintln!("Failed finding entry");
-        }
-    }
+    let mut entries = build_entries(&current_dir)?;
 
     let leading_path = current_dir.to_str().unwrap();
     // Reverse sort so that highest (most recent) mtimes are first
