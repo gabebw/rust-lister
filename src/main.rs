4fa65f8f73ed62c64dcd44b217d568fc213dfd90
Gabe Berke-Williams <gabebw@gabebw.com>
2020-07-29 08:47:49 -0700

    Allow sorting by creation time, too
    Bring in the clap crate for much nicer argument printing.

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,133 @@
+use clap::{App, Arg};
 use jwalk::{DirEntry, WalkDir};
-use num_cpus;
-use std::cmp::{self, Reverse};
-use std::env;
+use std::cmp::{min, Reverse};
 use std::error::Error;
 use std::fs::Metadata;
 use std::io;
-use std::path::{PathBuf};
+use std::path::PathBuf;
 use std::process::exit;
-use std::time::{SystemTime};
+use std::time::SystemTime;
 
-fn is_file(entry: &DirEntry) -> bool {
-    entry.file_type.as_ref().map(|f| f.is_file()).unwrap_or(false)
+#[derive(PartialEq, Eq)]
+enum SortBy {
+    Modified,
+    Created,
 }
 
-fn mtime_result(e: &DirEntry) -> Result<u64, Box<dyn Error>> {
+fn is_file(entry: &DirEntry) -> bool {
+    entry
+        .file_type
+        .as_ref()
+        .map(|f| f.is_file())
+        .unwrap_or(false)
+}
+
+fn metadata_result<F>(e: &DirEntry, process: F) -> Result<u64, Box<dyn Error>>
+where
+    F: Fn(&Metadata) -> io::Result<SystemTime>,
+{
     let metadata: Option<&Metadata> = e.metadata.as_ref().unwrap().as_ref().ok();
     if let Some(metadata) = metadata {
-        Ok(metadata.modified()?.duration_since(SystemTime::UNIX_EPOCH)?.as_secs())
+        Ok(process(metadata)?
+            .duration_since(SystemTime::UNIX_EPOCH)?
+            .as_secs())
     } else {
-        Err(Box::new(io::Error::new(io::ErrorKind::Other, "Couldn't get metadata")))
+        Err(Box::new(io::Error::new(
+            io::ErrorKind::Other,
+            "Couldn't get metadata",
+        )))
     }
 }
 
 fn mtime(e: &DirEntry, default: u64) -> u64 {
-    mtime_result(e).unwrap_or(default)
+    metadata_result(e, |metadata| metadata.modified()).unwrap_or(default)
 }
 
-fn build_entries(current_dir: &PathBuf, n: usize) -> Vec<DirEntry> {
+fn ctime(e: &DirEntry, default: u64) -> u64 {
+    metadata_result(e, |metadata| metadata.created()).unwrap_or(default)
+}
+
+fn build_entries(current_dir: &PathBuf, n: usize, sort_by: SortBy) -> Vec<DirEntry> {
     // Use a maximum of 4 threads. Never use more than half of the available CPU cores.
-    let num_threads = cmp::min(4, num_cpus::get()/2);
-    let walker = WalkDir::new(&current_dir).skip_hidden(true).preload_metadata(true).num_threads(num_threads);
-    let mut x: Vec<DirEntry> = walker.into_iter()
+    let num_threads = min(4, num_cpus::get() / 2);
+    let walker = WalkDir::new(&current_dir)
+        .skip_hidden(true)
+        .preload_metadata(true)
+        .num_threads(num_threads);
+    let mut x: Vec<DirEntry> = walker
+        .into_iter()
         // Skip items that we can't access
         .filter_map(Result::ok)
         // Skip directories
         .filter(is_file)
         .collect();
 
-    let now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
-    x.sort_by_cached_key(|e| Reverse(mtime(e, now)));
+    let now = SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
+    x.sort_by_cached_key(|e| match sort_by {
+        SortBy::Modified => Reverse(mtime(e, now)),
+        SortBy::Created => Reverse(ctime(e, now)),
+    });
     x.into_iter().take(n).collect()
 }
 
-fn maximum_number_of_entries_to_print(args: Option<&String>) -> usize {
+fn maximum_number_of_entries_to_print(args: Option<&str>) -> usize {
     match args {
         None => 10,
-        Some(s) => {
-            match s.parse() {
-                Err(_) => {
-                    eprintln!("Please pass a number as the first argument.");
-                    exit(1);
-                }
-                Ok(n) => n
+        Some(s) => match s.parse() {
+            Err(_) => {
+                eprintln!("Please pass a number to -n/--number-of-results.");
+                exit(1);
             }
-        }
+            Ok(n) => n,
+        },
     }
 }
 
-fn dir_to_look_at(dir: Option<&String>) -> PathBuf {
-    match dir {
-        Some(dir) => PathBuf::from(dir),
-        None => env::current_dir().unwrap()
-    }
+fn dir_to_look_at(dir: &str) -> PathBuf {
+    PathBuf::from(dir)
 }
 
 fn main() -> io::Result<()> {
-    let args: Vec<String> = env::args().collect();
-    let maximum_number_of_entries_to_print = maximum_number_of_entries_to_print(args.get(1));
-    let dir = dir_to_look_at(args.get(2));
-    let entries = build_entries(&dir, maximum_number_of_entries_to_print);
+    let matches = App::new("Lister")
+        .version("0.2")
+        .author("Gabe <gabebw@gabebw.com>")
+        .about("Recursively list files by most-recently-modified or -created times")
+        .arg(
+            Arg::with_name("sort-by")
+                .short("s")
+                .long("sort-by")
+                .value_name("SORT_BY")
+                .help("Sort by an attribute (defaults to modified)")
+                .takes_value(true)
+                .possible_values(&["modified", "created"]),
+        )
+        .arg(
+            Arg::with_name("NUMBER_OF_RESULTS")
+                .short("n")
+                .long("--number-of-results")
+                .value_name("N")
+                .help("Number of results to print (default 10)"),
+        )
+        .arg(
+            Arg::with_name("DIRECTORY")
+                .help("Directory to walk through (defaults to current directory)")
+                .index(1),
+        )
+        .get_matches();
+    let sort_by = match matches.value_of("sort-by").unwrap_or("modified") {
+        "created" => SortBy::Created,
+        "modified" => SortBy::Modified,
+        _ => SortBy::Modified,
+    };
 
+    let max_num_entries = maximum_number_of_entries_to_print(matches.value_of("NUMBER_OF_RESULTS"));
+    let dir = dir_to_look_at(matches.value_of("DIRECTORY").unwrap_or("."));
+    let entries = build_entries(&dir, max_num_entries, sort_by);
     let leading_path = dir.to_str().unwrap();
-    let number_of_entries_to_print = cmp::min(maximum_number_of_entries_to_print, entries.len());
+    let number_of_entries_to_print = min(max_num_entries, entries.len());
 
     for e in &entries[..number_of_entries_to_print] {
         let path = format!("{}", e.path().display());
